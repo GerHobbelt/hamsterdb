ff3b01680c4dcbe09c47cc50dcab4f4b9f07328e
Ger Hobbelt <ger@hobbelt.com>
2012-07-04 17:55:11 +0200

    wsclean script

---
+++ wsclean_run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# brute force everything to UNIX with spaces:
+wsclean -i -e -x -U -v  $( find . -type f -iname '*' -a ! -ipath '*/boost/*' -a ! -iname 'makefile*' )
+
+# make sure Makefiles are tabbed:
+wsclean -i -r -x -U -T 8 -v  $( find . -type f -iname 'Makefile*' -a ! -ipath '*/boost/*' )
+
+# and MSVC files are MSDOS:
+wsclean -i -r -x -W -v  $( find . -type f -iname '*.sln' -a ! -ipath '*/boost/*' )
+wsclean -i -r -x -W -v  $( find . -type f -iname '*.vc*proj' -a ! -ipath '*/boost/*' )
+wsclean -i -r -x -W -v  $( find . -type f -iname '*.def' -a ! -ipath '*/boost/*' )
+wsclean -i -r -x -W -v  $( find . -type f -iname '*.bat' -a ! -ipath '*/boost/*' )
+
